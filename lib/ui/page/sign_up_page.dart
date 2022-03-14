5b462cf0890a10ae4a9c09f141d6fffee5aa330b
unknown <iqbalgishafid@gmail.com>
2022-03-14 21:33:39 +0700

    add sign up page

---
+++ lib/ui/page/sign_up_page.dart
@@ -0,0 +1,24 @@
+part of 'pages.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(),
+      ),
+    );
+  }
+}
